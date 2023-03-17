5b944c9dd8909278124b809638dceeb3a0ca6585
MarianaKely <marianakely.r@gmail.com>
2023-03-16 22:26:14 -0300

    App page implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+ 
+ // General configuration and rendering the site - outset
+ 
+
  import MainConfiguration from "./components/MainConfiguration";
+ 
 
  export default function App (){
+
+
     return (
         
+// General configuration and rendering the site - outset
+
 
        <MainConfiguration></MainConfiguration>
 
+
+// General configuration and rendering the site - end   
+
+
     )
  }
+
+
+ // General configuration and rendering the site - end
+
+ 
