303fdb3e4f68782500fbc3aa8429661eb5f7ef61
MarianaKely <marianakely.r@gmail.com>
2023-03-16 22:27:41 -0300

    MyHabits text page -  incomplete

---
+++ src/pages/MyHabits.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function MyHabits () {
+  return (
+
+    <Text>
+      <p>TEXT</p>
+    </Text>
+
+  )
+}
+
+const Text = styled.div`
+width: 250px;
+height: 300px;
+background-color: pink;
+ p{
+  font-size: 50px;
+ }
+`
