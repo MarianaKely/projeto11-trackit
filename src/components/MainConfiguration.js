5a869cf79a4403293d9aa71ec0219da8d97b6743
MarianaKely <marianakely.r@gmail.com>
2023-03-16 22:23:43 -0300

    Main Configuration implemented

--- src/components/MainConfiguration.js
+++ src/components/MainConfiguration.js
@@ -1,27 +1,50 @@
 
+
+// General configuration and rendering the main config page - outset
+
+
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { UserContext } from "../global/UserContext";
 import styled from "styled-components";
+
+
+// Import files from js pages - outset
+
+import { UserContext } from "../global/UserContext";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
- import { hiddenHabits, listed } from "../global/Analysis";
- import TaskHistory from "../pages/TaskHistory";
- import TheDay from "../pages/TheDay";
- import MyHabits from "../pages/MyHabits";
+import { hiddenHabits, listed } from "../global/Analysis";
+import TaskHistory from "../pages/TaskHistory";
+import TheDay from "../pages/TheDay";
+import MyHabits from "../pages/MyHabits";
+
+
+// Import files from js pages - end
 
 
 
 
 export default function MainConfiguration () {
+
+
+// Constants and functions that enable functionalities on the header page - outset   
+  
+  
     const [profileName, setProfileName] = useState({});
     const [singleHabit, setSingleHabit] = useState([]);
     const [dailyschedule, setDailySchedule] = useState([]);
+
+
+// Constants and functions that enable functionalities on the header page - end    
     
   
     return (
 
-      <Container>
+
+// Creation configuration of header layout- outset    
+
+
+      <MainContainer>
 
 
         <UserContext.Provider value={{
@@ -52,15 +75,39 @@ export default function MainConfiguration () {
 
         </UserContext.Provider>
 
-      </Container>
+      </MainContainer>
 
-    );
+    )
+
+
+// Creation configuration of header layout- outset    
+
+
 
   }
+
+
+// MainConfiguration.js page styling - outset  
+
+
+// styling of the container that holds the whole site -outset
+
   
-  const Container = styled.div`
+  const MainContainer = styled.div`
+
     max-width: 614px;
     width: 100%;
-    hight: 100%;
-  `;
+    height: 100%;
+    background-color: #f2f2f2;
+
+  `
+
+
+// styling of the container that holds the whole site -end  
+
+
+// MainConfiguration.js page styling - end  
+
+
+// General configuration and rendering the main config page - outset
   
